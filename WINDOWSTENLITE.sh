6a51f6c19bb3e1919685efcca33a68640064f1ca
kingofnetflix <72701796+kingofnetflix@users.noreply.github.com>
2022-01-02 11:02:14 -0800

    updated qemu command to make vm faster

--- WINDOWSTENLITE.sh
+++ WINDOWSTENLITE.sh
@@ -15,5 +15,5 @@ curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*publ
 echo "Note: Use Right-Click Or Ctrl+C To Copy"
 echo "Please Keep Colab Tab Open, Maximum Time 12h"
 echo Script by fb.com/thuong.hai.581
-sudo qemu-system-x86_64 -vnc :0 -hda os.qcow2 -m 8G -enable-kvm -cpu host -smp cores=4 -machine usb=on -device usb-tablet -vga std > /dev/null 2>&1
+sudo qemu-system-x86_64 -vnc :0 -hda lite10.qcow2 -m 8G -enable-kvm -cpu host -smp cores=4 -machine usb=on -device usb-tablet -vga std > /dev/null 2>&1
 
