16f93b90d3321574e1015f9111e0862590e8295b
kingofnetflix <72701796+kingofnetflix@users.noreply.github.com>
2022-01-29 06:57:52 -0800

    Update neverinstallmc.sh

--- neverinstallmc.sh
+++ neverinstallmc.sh
@@ -1,4 +1,4 @@
 sudo apt update
 sudo apt install default-jdk -y
-wget -O minecraft.jar https://github.com/kingofnetflix/Linux-to-Windows-with-QEMU/blob/master/TLauncher-2.841.jar?raw=true
+wget -O minecraft.jar https://github.com/kingofnetflix/Linux-to-Windows-with-QEMU/blob/master/TLauncher-2.841.jar
 java -jar minecraft.jar
