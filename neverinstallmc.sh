6a568dba0b5354f3ce20599735371c58459f3756
kingofnetflix <72701796+kingofnetflix@users.noreply.github.com>
2022-01-29 06:46:20 -0800

    Create neverinstallmc.sh

---
+++ neverinstallmc.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt install default-jdk
+sudo apt install unzip
+wget https://dl2.tlauncher.org/f.php?f=files%2FTLauncher-2.841.zip
+unzip TLauncher-2.841.zip
+java -jar TLauncher-2.841.jar
