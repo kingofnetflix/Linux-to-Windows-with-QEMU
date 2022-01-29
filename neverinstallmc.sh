2fd13314d4c3325b2333d63439b710d33d2738e6
kingofnetflix <72701796+kingofnetflix@users.noreply.github.com>
2022-01-29 07:10:47 -0800

    Update neverinstallmc.sh

--- neverinstallmc.sh
+++ neverinstallmc.sh
@@ -1,4 +1,4 @@
 sudo apt update
 sudo apt install default-jdk -y
 wget -O minecraft.jar https://filebin.net/v44wz6b6k2e22ja9/TLauncher-2.841.jar
-java -jar minecraft.jar
+sudo java -jar minecraft.jar
