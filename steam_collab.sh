1924e67267027114d8b3457716427cc52b53eecf
kingofnetflix <72701796+kingofnetflix@users.noreply.github.com>
2022-02-04 10:57:39 -0800

    Update steam_collab.sh

--- steam_collab.sh
+++ steam_collab.sh
@@ -1,8 +1,6 @@
 wget https://cdn.akamai.steamstatic.com/client/installer/steam.deb
 sudo dpkg -i steam.deb
 sudo apt --fix-broken install
-sudo dpkg --add-architecture i386 
-sudo apt-get install libc6:i386 
-sudo apt-get install libgl1-mesa-glx-lts-trusty:i386
+sudo apt install libc6:amd64 libc6:i386 libegl1:amd64 libegl1:i386 libgbm1:amd64 libgbm1:i386 libgl1-mesa-dri:amd64 libgl1-mesa-dri:i386 libgl1:amd64 libgl1:i386 steam-libs-amd64:amd64 steam-libs-i386:i386 xdg-desktop-portal xdg-desktop-portal-gtk xterm
 sudo apt update
 steam
