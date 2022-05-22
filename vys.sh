6d491c6e7881fde81b4b4cf5e2c070b90892e856
kingofnetflix <72701796+kingofnetflix@users.noreply.github.com>
2022-05-21 21:02:48 -0500

    Create vys.sh

---
+++ vys.sh
@@ -0,0 +1,5 @@
+java -Xmx512M -Xms128M -jar ~/eaglercraft/java/bungee_command/bungee-dist.jar
+
+cd ~/eaglercraft/java/bukkit_command/
+
+java -Xmx3584M -Xms516M -jar ~/eaglercraft/java/bukkit_command/craftbukkit-1.5.2-R1.0.jar
